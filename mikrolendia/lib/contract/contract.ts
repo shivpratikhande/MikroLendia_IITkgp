a264231c98c6cf3d40034ff37c40d68576b7f2aa
shivv <shivpratikhande2017@gmail.com>
2025-01-18 15:34:40 +0530

    typo n type issue

--- mikrolendia/lib/contract/contract.ts
+++ mikrolendia/lib/contract/contract.ts
@@ -2,7 +2,7 @@ import { ethers } from 'ethers';
 import LoanContractABI from './config/LoanContractABI.json';
 import UserContractABI from './config/UserContractABI.json';
 import CommunityABI from "./config/CommunityAbi.json"
-import CommunityFactoryABI from "./config/communityFactoryABI.json"
+import CommunityFactoryABI from "./config/CommunityFactoryABI.json"
 const loanContractAddress = "0x4A1e8c5696a8B799D118AA3665ce0E75c64DB58b";
 const userContractAddress = "0x5D8d6f0D5632CdA559fFA5f7e9F5f66b2EDB011c";
 const communityFactoryAddress="0x08a7BA5De2e1B6272c8480D3b3A18b42C8951eBd";
@@ -18,6 +18,6 @@ export const getCommunityFactoryContract = (provider: ethers.providers.Provider)
   return new ethers.Contract(communityFactoryAddress, CommunityFactoryABI, provider);
 };
 
-export const getCommunityContract = (provider: ethers.providers.Provider, communityAddress: String) => {
+export const getCommunityContract = (provider: ethers.providers.Provider, communityAddress: string) => {
   return new ethers.Contract(communityAddress, CommunityABI, provider);
 };
